830f1ff0c31b1a2b7e7e44d74d52b688c6c29f8d
bingwan.gini.li@gmail.com <bingwan.gini.li@gmail.com>
2018-08-29 00:35:51 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,12 +1,1 @@
-var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
-
-function addElementToBeginningOfArray(array,element){
-  var array2 = (element, array)
-  return array
-}
-function destructivelyAddElementToBeginningOfArray(array,element){
-  var array = [1];
-  var element = "foo";
-  array.unshift(element);
-  return array
-}
+return Array
