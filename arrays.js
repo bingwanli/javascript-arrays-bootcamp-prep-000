e0bfbe5104a71e0ca294221626e37625b70d0859
bingwan.gini.li@gmail.com <bingwan.gini.li@gmail.com>
2018-08-29 00:40:54 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -7,3 +7,7 @@ function destructivelyAddElementToBeginningOfArray(array,element){
   array.unshift(element)
   return array
 }
+function addElementToBeginningOfArray(array,element){
+  return [array, ...element]
+}
+function
