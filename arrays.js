07f8a7a2e9835a3486ec5bf3cf932628aac96684
bingwan.gini.li@gmail.com <bingwan.gini.li@gmail.com>
2018-08-29 00:57:16 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -23,7 +23,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
 }
 function removeElementFromBeginningOfArray(array){
   array.slice(0)
-  return array
+ 
 }
 function destructivelyRemoveElementFromEndOfArray(array){
   array.slice(2)
