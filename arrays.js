4d32be51d581762108540fbe540a4ae7d436566b
bingwan.gini.li@gmail.com <bingwan.gini.li@gmail.com>
2018-08-29 00:57:47 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -22,7 +22,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
   return array
 }
 function removeElementFromBeginningOfArray(array){
-  array.slice(0)
+  return array.slice(0)
  
 }
 function destructivelyRemoveElementFromEndOfArray(array){
